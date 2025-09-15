441a2800665826eb8df194a5023ee81f0f80feb4
Baptiste Gosselin <baptiste.gosselin@etu.unicaen.fr>
2025-09-15 14:33:06 +0200

    Added: Exercices in JavaScript (1&2)

---
+++ Exercice1.js
@@ -0,0 +1,5 @@
+let celsius = 20;
+let farenheit = celsius * 9/5 + 32;
+
+console.log(celsius);
+console.log(farenheit);
---
+++ Exercice2.js
@@ -0,0 +1,7 @@
+const longueur = 10;
+const largeur = 5;
+const unite = "cm";
+
+const aire = longueur*largeur;
+
+console.log(aire + unite + "²");
